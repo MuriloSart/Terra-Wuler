601a77b78104b402f4463c4dfb6321bb2c02c4f0
Murilo Sartori <89427588+MuriloSart@users.noreply.github.com>
2025-01-25 16:31:28 -0300

    Feat SceneController and Singleton Class

---
+++ Assets/Scripts/Manager/SceneController.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class SceneController : Singleton<SceneController>
+{
+    private float Progress;
+
+    private void Start()
+    {
+        DontDestroyOnLoad(gameObject);
+    }
+
+    public void LoadScene(string nameScene)
+    {
+        SceneManager.LoadScene(nameScene);
+    }
+
+    public void LoadScreenAsync(string nameScene)
+    {
+        StartCoroutine(LoadLevelWithBar(nameScene));
+
+    }
+
+    IEnumerator LoadLevelWithBar(string nameScene)
+    {
+        SceneManager.LoadScene("Loading");
+
+        yield return new WaitForSecondsRealtime(3);
+
+        AsyncOperation async = SceneManager.LoadSceneAsync(nameScene, LoadSceneMode.Single);
+
+        Progress = 0;
+
+        while (!async.isDone)
+        {
+            Progress = Mathf.Clamp01(async.progress / .9f);
+            yield return null;
+        }
+
+    }
+
+    public float PegarProgresso()
+    {
+        return Progress;
+    }
+}
---
+++ Assets/Scripts/Patterns/Singleton.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+public class Singleton<T> : MonoBehaviour where T : MonoBehaviour
+{
+    public static T Instance;
+
+    public virtual void Awake()
+    {
+        if (Instance == null)
+            Instance = GetComponent<T>();
+        else
+            Destroy(gameObject);
+    }
+}
+
